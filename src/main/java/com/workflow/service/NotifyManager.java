65a5a1b762ff28fa6c92b4b95879bf36d02d4aa5
Prakash <omprakash4@deloitte.com>
2025-08-15 17:43:02 +0530

    milestone 3 partially done

---
+++ src/main/java/com/workflow/service/NotifyManager.java
@@ -0,0 +1,17 @@
+package com.workflow.service;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotifyManager implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        System.out.println("------------ ALERT ------------");
+        System.out.println("User : " + execution.getVariable("userName") + " with user ID : " + execution.getVariable("userId") + " is not completed within specified time period");
+        System.out.println("------------ ALERT ------------");
+
+//        call rest api to notify manager;
+    }
+}
